e84eddf0353efc5732440f34e067f8e72820f409
Tricky <tricky4724123@gmail.com>
2021-11-11 13:30:18 +0530

    done message

--- yt.py
+++ yt.py
@@ -7,19 +7,19 @@ sg.ChangeLookAndFeel('black')
 sg.theme('DarkBlue12')
 layout = [  [sg.Text('Youtube Downloader')],
             [sg.Text('Enter the video url'), sg.InputText()],
-            [sg.Text('Video will be saved in Videos folder')],
-            [sg.Button('Ok'), sg.Button('Exit')]
+            [sg.Text(':)', key='done')],
+            [sg.Button('Start'), sg.Button('Exit')]
         ]
 window = sg.Window('yt_downloader', layout, icon='./yt.ico')
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == 'Ok':
+    if event == 'Start':
         url = values[0]
         yt = YouTube(url)
         yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(path + "\\Videos")
-        layout.insert(3, "[sg.Text('Done!')]")
+        window['done'].update('Done!')
 
 window.close()
 
